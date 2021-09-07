23d1492caad4a9f12c00326468c076f636d1cf2b
João Henrique Santana <joaoenrique.san@gmail.com>
2021-09-07 15:09:24 -0300

    Add basic functions

--- js/calculator.js
+++ js/calculator.js
@@ -1,1 +1,15 @@
-console.log("Hello World !");
+function add(n1, n2){
+    return n1 + n2;
+}
+function subtract(n1, n2){
+    return n1 - n2;
+}
+function multiply(n1,n2){
+    return n1 * n2;
+}
+function divide(n1, n2){
+    return n1 / n2;
+}
+function operate(operation, n1, n2){
+    return operation(n1,n2);
+}
