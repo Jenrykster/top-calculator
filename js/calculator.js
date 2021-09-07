a1adbc30e862062b3e48d01486e173f3dfc0f9e2
João Henrique Santana <joaoenrique.san@gmail.com>
2021-09-07 16:33:19 -0300

    Add function to number buttons

--- js/calculator.js
+++ js/calculator.js
@@ -19,6 +19,21 @@ buttons.forEach((button)=>{
     button.addEventListener('click', onButtonClick)
 })
 
+const display = document.querySelector('#display h3');
+
 function onButtonClick(e){
-    console.log(e.target);
+    if(display.innerHTML == '0'){
+        display.innerHTML = e.target.innerHTML; 
+    }else{
+        display.innerHTML += e.target.innerHTML;
+    }
+    if(display.innerHTML.includes("OVERLOAD")){
+        display.innerHTML = '0';
+    }
+    if(display.innerHTML.length >= 14){
+        display.innerHTML = "OVERLOAD";
+    }
+    if(e.target.id == "clear"){
+        display.innerHTML = '0';
+    }
 }
