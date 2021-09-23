e30c33cf9641b48086867e047db7c166e536f535
João Henrique Santana <joaoenrique.san@gmail.com>
2021-09-23 14:18:40 -0300

    Fix issue with multiple operations

--- js/calculator.js
+++ js/calculator.js
@@ -8,6 +8,7 @@ function multiply(n1,n2){
     return n1 * n2;
 }
 function divide(n1, n2){
+    if(n2 == 0 ) return "ERR0R/0/0"
     return n1 / n2;
 }
 function operate(operation, n1, n2){
@@ -21,19 +22,58 @@ buttons.forEach((button)=>{
 
 const display = document.querySelector('#display h3');
 
+let n1 = 0;
+let n2 = 0;
+let operation; 
+let lastResult;
 function onButtonClick(e){
-    if(display.innerHTML == '0'){
-        display.innerHTML = e.target.innerHTML; 
-    }else{
-        display.innerHTML += e.target.innerHTML;
+    if(e.target.innerHTML == '=' && display.innerHTML == lastResult){
+        return;
     }
-    if(display.innerHTML.includes("OVERLOAD")){
+    if(e.target.classList.contains('symbol')){
+        if(n1 == 0){
+            n1 = parseInt(display.innerHTML);
+            display.innerHTML = '0';
+        }else if(n2 == 0 || n1 == lastResult){
+            n2 = parseInt(display.innerHTML);
+            display.innerHTML = '0';
+        }   
+        switch (e.target.innerHTML){
+            case '=':
+                console.log("N1:", n1, "\nN2:", n2,"\nLast Result:", lastResult);
+                lastResult = operate(operation, n1, n2);
+                if(lastResult.toString().length > 12) lastResult = lastResult.toFixed(4);
+                display.innerHTML = lastResult;
+                n1 = lastResult;
+                break
+            case '+':
+                operation = add;
+                break;
+            case '/':
+                operation = divide;
+                break;
+            case '-':
+                operation = subtract;
+                break;
+            case 'X':
+                operation = multiply;
+                break;  
+            case 'AC':
+                n1 = 0;
+                n2 = 0;
+                lastResult = 0;  
+        }
+        
+    }
+    else{
+        display.innerHTML == 0 ? display.innerHTML = e.target.innerHTML : display.innerHTML += e.target.innerHTML;
+    }
+    if(display.innerHTML.includes("OVERLOAD") ||display.innerHTML.includes("undefined")  || e.target.id == "clear"){
+        n1, n2 = 0, 0;
         display.innerHTML = '0';
     }
     if(display.innerHTML.length >= 14){
         display.innerHTML = "OVERLOAD";
     }
-    if(e.target.id == "clear"){
-        display.innerHTML = '0';
-    }
+    
 }
